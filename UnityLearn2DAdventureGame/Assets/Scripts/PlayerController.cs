26c3eb1d080e5a9083d497e4fbc410caa04f573a
Samet Kosmaz <ksmzsmt@gmail.com>
2024-02-12 18:08:24 +0300

    update V2

--- UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
+++ UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
@@ -5,38 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+    public InputAction MoveAction;
+    private void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-
-        Debug.Log(vertical);
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move=MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position+move*3.0f*Time.deltaTime;
         transform.position = position;
     }
 }
