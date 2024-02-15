a11dce2f19c3725c7b69a53b386f7dee956ea177
Samet Kosmaz <ksmzsmt@gmail.com>
2024-02-15 17:48:02 +0300

    update V3

--- UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
+++ UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,33 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    private Rigidbody2D rigidbody2d;
+    public float speed = 3.0f;
+    private Vector2 move;
+    public int maxHealth=5;
+    private int currentHealth;
     private void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
-        Vector2 move=MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position+move*3.0f*Time.deltaTime;
-        transform.position = position;
+        move=MoveAction.ReadValue<Vector2>();
+        Debug.Log(move); 
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
