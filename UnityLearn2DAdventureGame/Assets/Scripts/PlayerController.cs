03118ab77c801d1a7455a7efedb2b9c662239833
Samet Kosmaz <ksmzsmt@gmail.com>
2024-02-16 18:28:02 +0300

    update V5

--- UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
+++ UnityLearn2DAdventureGame/Assets/Scripts/PlayerController.cs
@@ -59,6 +59,6 @@ public class PlayerController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
     }
 }
